cc4651d11906c67590eb15ff4f07d0bb1de75736
Patryk Dolata <patrykdolata@gmail.com>
2018-03-16 20:29:23 +0100

    #92: Correct welcome scenario.

--- src/test/java/cucumber/steps/welcome/helloWorldSteps.java
+++ src/test/java/cucumber/steps/welcome/helloWorldSteps.java
@@ -5,8 +5,8 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class helloWorldSteps {
-	@Given("^that site adress is \'(.*)\'$")
-	public void thatSiteAdressIs(String url) {
+	@Given("^that front page adress is \'(.*)\'$")
+	public void thatFrontPageAdressIs(String url) {
 		
 	}
 
--- src/test/resources/features/welcome/helloWorld.feature
+++ src/test/resources/features/welcome/helloWorld.feature
@@ -1,6 +1,6 @@
 Feature: Hello World
 
   Scenario: Which I see welcome message on front page 
-    Given that site adress is 'http://localhost:8080/'
+    Given that front page adress is 'http://localhost:8080/'
     When I run front page
     Then site will be contain text 'Hello World!'
